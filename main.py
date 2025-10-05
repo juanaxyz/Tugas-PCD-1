5c5a8affa84dd110af605e571091fed9ed499bd0
juanaxyz <inijuana@gmail.com>
2025-10-05 12:08:47 +0800

    update: main.py : melengkapi main function dengan pilihan operasi yang ingin dilakukan
    add :
    output folder untuk menyimpan gambar terbaru hasil operasi

--- main.py
+++ main.py
@@ -3,36 +3,73 @@ import math
 
 # Load gambar
 img = Image.open("./images/baymax(1).jpg")
-
 px = img.load()
-print(px)
+
+
+def cek_tipe_gambar():
+    print(px[0, 0])
+    try:
+        r, g, b = px[0, 0]
+        print(r, g, b)
+        return "RGB"
+    except TypeError:
+        return "L"
+
+
+mode = cek_tipe_gambar()
+if mode == "RGB":
+    canvas = Image.new("RGB", (img.width, img.height), (255, 255, 255))
+else:
+    canvas = Image.new("L", (img.width, img.height), 255)
+
+
+def tampilkan_gambar_asli():
+    img.show(title="Gambar Asli")
+    return img
+
+
+def save_gambar():
+    if latest_image is not None:
+        latest_image.save("./output/hasil.jpg")
+        print("Gambar berhasil disimpan sebagai 'hasil.jpg'")
+    else:
+        print("Tidak ada gambar hasil yang bisa disimpan.")
+
 
 def translasi(s_x: int, s_y: int) -> Image:
     """
     Fungsi untuk melakukan translasi gambar
     Args :
-    s_x (int), s_y (int): nilai translasi dalam sumbu x dan y
+    s_x (int) : nilai translasi dalam sumbu x
+    s_y (int) : nilai translasi dalam sumbu y
     Returns : Image
     """
-    canvas_Translasi = Image.new("RGB", (img.width, img.height), (255, 255, 255))
-    px_new = canvas_Translasi.load()
+    if mode == "RGB":
+        temp_canvas = Image.new("RGB", (img.width, img.height), (255, 255, 255))
+    else:
+        temp_canvas = Image.new("L", (img.width, img.height), 255)
+    px_new = temp_canvas.load()
 
     # Pindahkan pixel sesuai rumus translasi
     for y in range(img.height):
         for x in range(img.width):
             new_x, new_y = x + s_x, y + s_y
             if 0 <= new_x < img.width and 0 <= new_y < img.height:
-                px_new[x + s_x, y + s_y] = px[x, y]
+                px_new[new_x, new_y] = px[x, y]
 
-    canvas_Translasi.show()  ## Tampilkan hasil
+    temp_canvas.show()  ## Tampilkan hasil
+    return temp_canvas
 
 
-def perbesaran(x: int) -> Image:
+def perbesaran(sx: int, sy: int) -> Image:
     # perbesaran
-    s_x, s_y = x, x
+    s_x, s_y = sx, sy
     new_w, new_h = img.width * s_x, img.height * s_y
-    canvas_perbesaran = Image.new("RGB", [new_w, new_h], (0, 0, 0))
-    px_baru = canvas_perbesaran.load()
+    if mode == "RGB":
+        canvas = Image.new("RGB", [new_w, new_h], (0, 0, 0))
+    else:
+        canvas = Image.new("L", [new_w, new_h], 255)
+    px_baru = canvas.load()
 
     for x in range(new_w):
         for y in range(new_h):
@@ -40,12 +77,12 @@ def perbesaran(x: int) -> Image:
             src_y = int(y / s_y)
             px_baru[x, y] = px[src_x, src_y]
 
-    canvas_perbesaran.show()  # show image perbesaran
+    canvas.show()  # show image perbesaran
+    return canvas
 
 
 def pencerminan_x() -> Image:
     center = (img.width - 1) / 2
-    canvas = Image.new("RGB", [img.width, img.height], (0, 0, 0))
     px_new = canvas.load()
 
     for x in range(img.width):
@@ -54,11 +91,11 @@ def pencerminan_x() -> Image:
             px_new[x, y] = px[xb, y]
 
     canvas.show()  # image pencerminan
+    return canvas
 
 
 def pencerminan_y() -> Image:
     center = (img.height - 1) / 2
-    canvas = Image.new("RGB", [img.width, img.height], (0, 0, 0))
     px_new = canvas.load()
 
     for x in range(img.width):
@@ -67,10 +104,10 @@ def pencerminan_y() -> Image:
             px_new[x, y] = px[x, yb]
 
     canvas.show()  # image pencerminan
+    return canvas
 
 
 def pencerminan_kombinasi() -> Image:
-    canvas = Image.new("RGB", [img.width, img.height], (0, 0, 0))
     px_new = canvas.load()
 
     for x in range(img.width):
@@ -80,6 +117,7 @@ def pencerminan_kombinasi() -> Image:
             px_new[x, y] = px[xb, yb]
 
     canvas.show()  # image pencerminan
+    return canvas
 
 
 def rotate(degree: int) -> Image:
@@ -137,14 +175,15 @@ def rotate(degree: int) -> Image:
 def crop(xL: int, xR: int, yT: int, yB: int):
     wb = xR - xL
     hb = yB - yT
-    canvas = Image.new("RGB", [wb, hb], (255, 255, 255))
-    px_new = canvas.load()
+    canvas_crop = Image.new("RGB", [wb, hb], (255, 255, 255))
+    px_new = canvas_crop.load()
 
     for x in range(wb):
         for y in range(hb):
             px_new[x, y] = px[x + xL, y + yT]
 
-    canvas.show()
+    canvas_crop.show()
+    return canvas_crop
 
 
 def affine(matrix, translasi=(0, 0)) -> Image:
@@ -161,7 +200,6 @@ def affine(matrix, translasi=(0, 0)) -> Image:
     print(matrix[0])
     cx, cy = img.width / 2, img.height / 2
 
-    canvas = Image.new("RGB", (img.width, img.height), (255, 255, 255))
     px_new = canvas.load()
 
     for x in range(img.width):
@@ -199,7 +237,6 @@ def ripple(ax: int = 10, ay: int = 10, Tx: int = 50, Ty: int = 50) -> Image:
     Returns:
         Image: Gambar hasil transformasi ripple.
     """
-    canvas = Image.new("RGB", (img.width, img.height), (255, 255, 255))
     px_new = canvas.load()
 
     for y_ in range(img.height):
@@ -218,6 +255,11 @@ def ripple(ax: int = 10, ay: int = 10, Tx: int = 50, Ty: int = 50) -> Image:
 # operasi enhancement
 
 
+def hitung_nilai_gray(r: int, g: int, b: int) -> int:
+    return int(0.299 * r + 0.587 * g + 0.114 * b)
+    # return int((r + g + b ) // 3)
+
+
 def rgb_to_grayscale():
     canvas = Image.new("L", (img.width, img.height))
     px_new = canvas.load()
@@ -225,8 +267,7 @@ def rgb_to_grayscale():
     for y in range(img.height):
         for x in range(img.width):
             r, g, b = px[x, y]
-            # gray = (r + g + b ) // 3
-            gray = int(0.299 * r + 0.587 * g + 0.114 * b)
+            gray = hitung_nilai_gray(r, g, b)
             px_new[x, y] = gray
 
     # canvas.show()
@@ -234,141 +275,229 @@ def rgb_to_grayscale():
 
 
 def grayscale_to_biner():
-    canvas = Image.new("1", (img.width, img.height))
-    px_new = canvas.load()
+    canvas_biner = Image.new("1", (img.width, img.height))
+    px_new = canvas_biner.load()
+
+    T = 128  # threesold
 
     for x in range(img.width):
         for y in range(img.height):
             r, g, b = px[x, y]
-            gray = int(0.299 * r + 0.587 * g + 0.114 * b)
-            T = 128
+            gray = hitung_nilai_gray(r, g, b)
             if gray >= T:
                 px_new[x, y] = 255  # putih
             else:
                 px_new[x, y] = 0  # hitam
-    canvas.show()
-    return canvas
+    canvas_biner.show()
+    return canvas_biner
 
 
 def double_thresholding(T1: int, T2: int):
-    canvas = Image.new("1", (img.width, img.height))
-    px_new = canvas.load()
+    canvas_biner = Image.new("1", (img.width, img.height))
+    px_new = canvas_biner.load()
 
     for x in range(img.width):
         for y in range(img.height):
             # print(px[x,y])
             r, g, b = px[x, y]
-            gray = int(0.299 * r + 0.587 * g + 0.114 * b)
+            gray = hitung_nilai_gray(r, g, b)
             if T1 <= gray <= T2:
                 # px_new[x, y] = 0  # black
                 px_new[x, y] = 255
             else:
                 # px_new[x, y] = 255  # white
                 px_new[x, y] = 0
-    canvas.show()
-    return canvas
+    canvas_biner.show()
+    return canvas_biner
 
 
 def RGB_to_mbit(m: int):
-    canvas = Image.new("L", (img.width, img.height))
-    px_new = canvas.load()
+    canvas_mbit = Image.new("L", (img.width, img.height))
+    px_new = canvas_mbit.load()
 
     for y in range(img.height):
         for x in range(img.width):
             r, g, b = px[x, y]
-            # gray = (r + g + b ) // 3
-            gray = int(0.299 * r + 0.587 * g + 0.114 * b)
-            p_baru = (2**(8-m+1)) * int(gray/2**(8-m+1))
+            gray = hitung_nilai_gray(r, g, b)
+            p_baru = (2 ** (8 - m + 1)) * int(gray / 2 ** (8 - m + 1))
             px_new[x, y] = p_baru
 
-    canvas.show()
-    return canvas
+    canvas_mbit.show()
+    return canvas_mbit
 
-def  change_brigthness(c: int):
-    canvas = Image.new("RGB", (img.width, img.height))
+
+def change_brigthness(c: int):
     px_new = canvas.load()
 
     for y in range(img.height):
         for x in range(img.width):
-            if len(px[x,y]) == 3:
+            if len(px[x, y]) == 3:
                 r, g, b = px[x, y]
                 r_baru = r + c
                 g_baru = g + c
-                b_baru =  b + c
+                b_baru = b + c
                 px_new[x, y] = (r_baru, g_baru, b_baru)
-            else : 
-                gray = px[x,y]
+            else:
+                gray = px[x, y]
                 gray_baru = gray + c
-                px_new[x,y] = gray_baru
+                px_new[x, y] = gray_baru
     canvas.show()
     return canvas
 
-def change_kontras(faktor : float):
-    if type(px[0,0]) is tuple:
-        canvas = Image.new("RGB", (img.width, img.height))
-        px_new = canvas.load()
-    else :
-        canvas = Image.new("L", (img.width, img.height))
-        px_new = canvas.load()
+
+def change_kontras(faktor: float):
+    px_new = canvas.load()
 
     for y in range(img.height):
         for x in range(img.width):
-            # print(type(px[x,y]))
-            if type(px[x,y]) is tuple:
-                
+            # cek gambar rgb atau grayscale
+            if type(px[x, y]) is tuple:
                 r, g, b = px[x, y]
-                r_baru = int(faktor * (r-128) + 128)
-                g_baru = int(faktor * (g-128) + 128)
-                b_baru = int(faktor * (b-128) + 128)
+                r_baru = int(faktor * (r - 128) + 128)
+                g_baru = int(faktor * (g - 128) + 128)
+                b_baru = int(faktor * (b - 128) + 128)
                 px_new[x, y] = (r_baru, g_baru, b_baru)
-            else : 
-                gray = px[x,y]
-                gray_baru = int(faktor * (gray-128) + 128)
-                px_new[x,y] = gray_baru
+            else:
+                gray = px[x, y]
+                gray_baru = int(faktor * (gray - 128) + 128)
+                px_new[x, y] = gray_baru
     canvas.show()
     return canvas
 
+
 def negasi():
-    if type(px[0,0]) is tuple:
-        canvas = Image.new("RGB", (img.width, img.height))
-        px_new = canvas.load()
-    else :
-        canvas = Image.new("L", (img.width, img.height))
-        px_new = canvas.load()
-    
+    px_new = canvas.load()
     for x in range(img.width):
         for y in range(img.height):
-            if type(px[x,y]) is tuple:
+            if type(px[x, y]) is tuple:
                 r, g, b = px[x, y]
                 r_baru = 255 - r
                 g_baru = 255 - g
                 b_baru = 255 - b
                 px_new[x, y] = (r_baru, g_baru, b_baru)
-            else : 
-                gray = px[x,y]
+            else:
+                gray = px[x, y]
                 gray_baru = 255 - gray
-                px_new[x,y] = gray_baru
+                px_new[x, y] = gray_baru
     canvas.show()
+    return canvas
+
+
+latest_image = None
 
 if __name__ == "__main__":
     # print resolusi gambar
     print(f"width original : {img.width}")
     print(f"height original : {img.height}")
 
-    gray = rgb_to_grayscale()
-    px = gray.load()
-    # gray.show()
-    # img.show()
-    # rotasi matrix dengan T affine
-    # affine(hitung_matrix_rotation(180), translasi=(0, 0))
-    # ripple_manual(5,5,200,100)
-    # ripple(100,20, 200,100)
-    # crop(100,400,100,400)
-    # pencerminan_kombinasi()
-    # double_thresholding(100, 300)
-    # RGB_to_mbit(3)
-    # change_brigthness(100)
-    # rgb_to_grayscale()
-    # grayscale_to_biner()
-    # change_kontras(1.5)
-    negasi()
+    while True:
+        print("\nMenu Operasi Citra:")
+        print("1. Translasi")
+        print("2. Perbesaran")
+        print("3. Pencerminan X")
+        print("4. Pencerminan Y")
+        print("5. Pencerminan Kombinasi")
+        print("6. Rotasi")
+        print("7. Crop")
+        print("8. Affine Transformasi (Rotasi)")
+        print("9. Ripple")
+        print("10. RGB ke Grayscale")
+        print("11. Grayscale ke Biner")
+        print("12. Double Thresholding")
+        print("13. RGB ke m-bit")
+        print("14. Ubah Brightness")
+        print("15. Ubah Kontras")
+        print("16. Negasi")
+        print("98. Save Gambar Terbaru")
+        print("99. Tampilkan Gambar Asli")
+        print("0. Keluar")
+        pilihan = input("Pilih Operasi : ")
+        match pilihan:
+            case "1":
+                sx = int(input("Masukkan translasi X: "))
+                sy = int(input("Masukkan translasi Y: "))
+                latest_image = translasi(sx, sy)
+            case "2":
+                sx = int(input("Masukkan faktor perbesaran X: "))
+                sy = int(input("Masukkan faktor perbesaran Y: "))
+                latest_image = perbesaran(sx, sy)
+            case "3":
+                latest_image = pencerminan_x()
+            case "4":
+                latest_image = pencerminan_y()
+            case "5":
+                latest_image = pencerminan_kombinasi()
+            case "6":
+                deg = int(input("Masukkan derajat rotasi: "))
+                latest_image = rotate(deg)
+            case "7":
+                xL = int(input("x Left: "))
+                xR = int(input("x Right: "))
+                yT = int(input("y Top: "))
+                yB = int(input("y Bottom: "))
+                latest_image = crop(xL, xR, yT, yB)
+            case "8":
+                print("Affine Transformasi:")
+                print("1. Rotasi (masukkan derajat)")
+                print("2. Scaling (sx, sy)")
+                print("3. Shear (kx, ky)")
+
+                sub = input("Pilih jenis transformasi: ")
+
+                if sub == "1":  # rotasi
+                    deg = float(input("Masukkan derajat rotasi: "))
+                    matrix = hitung_matrix_rotation(deg)
+                    e, f = 0, 0
+
+                elif sub == "2":  # scaling
+                    sx = float(input("Masukkan faktor skala X: "))
+                    sy = float(input("Masukkan faktor skala Y: "))
+                    matrix = [[sx, 0], [0, sy]]
+                    e, f = 0, 0
+
+                elif sub == "3":  # shear
+                    kx = float(input("Masukkan faktor shear X: "))
+                    ky = float(input("Masukkan faktor shear Y: "))
+                    matrix = [[1, kx], [ky, 1]]
+                    e, f = 0, 0
+
+                else:  # custom
+                    print("tidak valid")
+
+                latest_image = affine(matrix, translasi=(e, f))
+
+            case "9":
+                ax = int(input("ax: "))
+                ay = int(input("ay: "))
+                Tx = int(input("Tx: "))
+                Ty = int(input("Ty: "))
+                latest_image = ripple(ax, ay, Tx, Ty)
+            case "10":
+                latest_image = rgb_to_grayscale()
+                latest_image.show()
+            case "11":
+                latest_image = grayscale_to_biner()
+            case "12":
+                T1 = int(input("Threshold bawah: "))
+                T2 = int(input("Threshold atas: "))
+                latest_image = double_thresholding(T1, T2)
+            case "13":
+                m = int(input("Masukkan nilai m (1-8): "))
+                latest_image = RGB_to_mbit(m)
+            case "14":
+                c = int(input("Masukkan perubahan brightness: "))
+                latest_image = change_brigthness(c)
+            case "15":
+                f = float(input("Masukkan faktor kontras: "))
+                latest_image = change_kontras(f)
+            case "16":
+                latest_image = negasi()
+            case "98":
+                save_gambar()
+            case "99":
+                tampilkan_gambar_asli()
+            case "0":
+                print("Keluar.")
+                break
+            case _:
+                print("Pilihan tidak valid.")
